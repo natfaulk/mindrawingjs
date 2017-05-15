50363269575e5a2113af12e6fa961215b007e592
Nathaniel Faulkner <nfaulk@hotmail.co.uk>
2017-05-15 13:49:30 +1200

    Can now have multiple instances

--- src/mindrawing.js
+++ src/mindrawing.js
@@ -1,46 +1,52 @@
-mindrawingjs = {
-  setup: function(canvasID, width, height) {
-    this.c = document.getElementById(canvasID);
-    this.ctx = this.c.getContext('2d');
+mindrawingjs = function () {};
 
-    if (arguments.length == 3) {
-      this.c.width = width;
-      this.c.height = height;
-    }
+mindrawingjs.prototype.setup = function (canvasID, width, height) {
+  this.c = document.getElementById(canvasID);
+  this.ctx = this.c.getContext('2d');
 
-    this.width = this.c.width;
-    this.height = this.c.height;
-  },
-  background: function(colour) {
-    this.ctx.fillStyle = colour;
-    this.ctx.fillRect(0, 0, this.width, this.height);
-  },
-  line: function(ax, ay, bx, by) {
-    // so 1px lines are crisp, else they are spread over 2 px width and half the intensity
-    if (this.ctx.lineWidth == 1) {
-      ax += 0.5;
-      ay += 0.5;
-      bx += 0.5;
-      by += 0.5;
-    }
-    this.ctx.beginPath();
-    this.ctx.moveTo(ax, ay);
-    this.ctx.lineTo(bx, by);
-    this.ctx.stroke();
-  },
-  stroke: function(colour) {
-    this.ctx.strokeStyle = colour;
-  },
-  strokeWeight: function(weight) {
-    this.ctx.lineWidth = weight;
-  },
-  fill: function(colour) {
-    this.ctx.fillStyle = colour;
-  },
-  rect: function(ax, ay, w, h) {
-    this.ctx.beginPath();
-    this.ctx.rect(ax, ay, w, h);
-    this.ctx.fill();
-    this.ctx.stroke();
+  if (arguments.length == 3) {
+    this.c.width = width;
+    this.c.height = height;
   }
+
+  this.width = this.c.width;
+  this.height = this.c.height;
+};
+
+mindrawingjs.prototype.background = function(colour) {
+  this.ctx.fillStyle = colour;
+  this.ctx.fillRect(0, 0, this.width, this.height);
+};
+
+mindrawingjs.prototype.line = function(ax, ay, bx, by) {
+  // so 1px lines are crisp, else they are spread over 2 px width and half the intensity
+  if (this.ctx.lineWidth == 1) {
+    ax += 0.5;
+    ay += 0.5;
+    bx += 0.5;
+    by += 0.5;
+  }
+  this.ctx.beginPath();
+  this.ctx.moveTo(ax, ay);
+  this.ctx.lineTo(bx, by);
+  this.ctx.stroke();
+};
+
+mindrawingjs.prototype.stroke = function(colour) {
+  this.ctx.strokeStyle = colour;
+};
+
+mindrawingjs.prototype.strokeWeight = function(weight) {
+  this.ctx.lineWidth = weight;
+};
+
+mindrawingjs.prototype.fill = function(colour) {
+  this.ctx.fillStyle = colour;
+};
+
+mindrawingjs.prototype.rect = function(ax, ay, w, h) {
+  this.ctx.beginPath();
+  this.ctx.rect(ax, ay, w, h);
+  this.ctx.fill();
+  this.ctx.stroke();
 };
