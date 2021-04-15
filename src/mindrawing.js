c3a9c5d112345e142f132bb6db995a4542acdb29
natfaulk <nfaulk@hotmail.co.uk>
2021-04-15 12:40:35 +1200

    fix broken bezier function

--- src/mindrawing.js
+++ src/mindrawing.js
@@ -44,8 +44,8 @@ Mindrawingjs = function () {};
 
   Mindrawingjs.prototype.bezier = function(ax, ay, bx, by, h1x, h1y, h2x, h2y) {
     this.ctx.beginPath();
-    d.ctx.moveTo(ax, ay);
-    d.ctx.bezierCurveTo(h1x, h1y, h2x, h2y, bx, by);
+    this.ctx.moveTo(ax, ay);
+    this.ctx.bezierCurveTo(h1x, h1y, h2x, h2y, bx, by);
     this.ctx.stroke();
   };
 
